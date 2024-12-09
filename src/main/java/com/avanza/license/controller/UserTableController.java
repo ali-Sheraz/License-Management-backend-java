1c3ea4c9de7050a1a78bc8b678141180270262e3
Rizwan <Rizwan@avanzasolutions.com>
2024-12-09 14:16:45 +0500

    modify the register and update Api for license

--- src/main/java/com/avanza/license/controller/UserTableController.java
+++ src/main/java/com/avanza/license/controller/UserTableController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v2")
+@RequestMapping("/v1")
 public class UserTableController {
 
     @Autowired
