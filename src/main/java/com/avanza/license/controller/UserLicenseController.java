a780a1dc1661e93cfea51b4339076e6678133c4e
Rizwan <Rizwan@avanzasolutions.com>
2024-12-18 12:53:55 +0500

    secure api role wise

--- src/main/java/com/avanza/license/controller/UserLicenseController.java
+++ src/main/java/com/avanza/license/controller/UserLicenseController.java
@@ -15,47 +15,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1")
 public class UserLicenseController {
 
     @Autowired
     private UserLicenseService userLicenseService;
 
-    @GetMapping("/userLicense/{userId}/{appId}")
+    @GetMapping("/v1/userLicense/{userId}/{appId}")
     public ResponseEntity<UserLicense> getUserLicenseByUserIdAndAppId(@PathVariable Long userId, @PathVariable Long appId) {
         UserLicense userLicense = userLicenseService.getLicenseByUserIdAndAppId(userId, appId);
         return new ResponseEntity<>(userLicense, HttpStatus.OK);
 
     }
 
-    @GetMapping("/userLicense")
+    @GetMapping("/v1/userLicense")
     public ResponseEntity<List<UserLicense>> getAllUserLicense() {
         List<UserLicense> userLicenses = userLicenseService.getAllUserLicenses();
         return new ResponseEntity<>(userLicenses, HttpStatus.OK);
     }
-    @GetMapping("/userLicenseKey")
+    @GetMapping("/v3/userLicenseKey")
     public ResponseEntity<UserLicenseFloatAbleDTO> isLicenseKeyValid(@RequestParam("userId") Long userId, @RequestParam("appId") Long appId, @RequestParam("loginId") String loginId, @RequestParam("keyValue") String keyValue) {
         UserLicenseFloatAbleDTO UserLicense = userLicenseService.isLicenseKeyValid(userId, appId,loginId, keyValue);
         return new ResponseEntity<>(UserLicense, HttpStatus.OK);
     }
 
-    @PostMapping("/userLicenseFloatAbleMatrix")
+    @PostMapping("/v3/userLicenseFloatAbleMatrix")
     public ResponseEntity<UserLicenseFloatAbleDTO> isLicenseKeyValidForFloatAbleMatrix(@RequestBody LicenseRequestParam licenseRequestParam) {
         UserLicenseFloatAbleDTO UserLicense = userLicenseService.isLicenseKeyValidForFloatAbleMatrix(licenseRequestParam);
         return new ResponseEntity<>(UserLicense, HttpStatus.OK);
     }
-    @PostMapping("/isLicenseExpiredUser")
+    @PostMapping("/v3/isLicenseExpiredUser")
     public ResponseEntity<IsLicenseExpiredDTO> isLicenseExpired(@RequestBody LicenseRequestParam licenseRequestParam) {
         IsLicenseExpiredDTO UserLicense = userLicenseService.isLicenseExpired(licenseRequestParam);
         return new ResponseEntity<>(UserLicense, HttpStatus.OK);
     }
 
-    @GetMapping("/insertingSession/{userId}/{appId}/{loginId}")
+    @GetMapping("/v3/insertingSession/{userId}/{appId}/{loginId}")
     public ResponseEntity<SessionDTO> insertingSession(@PathVariable Long userId, @PathVariable Long appId, @PathVariable String loginId) {
         SessionDTO userSession = userLicenseService.insertingSession(userId, appId, loginId);
         return new ResponseEntity<>(userSession, HttpStatus.OK);
     }
-    @GetMapping("/userLicense/{userId}")
+    @GetMapping("/v1/userLicense/{userId}")
     public ResponseEntity<List<UserLicense>> getUserLicenseByUserId(@PathVariable Long userId) {
         List<UserLicense> userLicenses = userLicenseService.getUserLicenseByUserId(userId);
         return new ResponseEntity<>(userLicenses, HttpStatus.OK);
--- src/main/java/com/avanza/license/controller/UserSessionController.java
+++ src/main/java/com/avanza/license/controller/UserSessionController.java
@@ -10,39 +10,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1")
 public class UserSessionController {
 
     @Autowired
     private UserSessionService userSessionService;
 
-    @PostMapping("/userSession/{userId}/{appId}")
+    @PostMapping("/v1/userSession/{userId}/{appId}")
     public ResponseEntity<UserSession> saveUserSession(@RequestBody UserSession userSession, @PathVariable Long userId, @PathVariable Long appId) {
         UserSession savedSession = userSessionService.saveUserSession(userSession, userId, appId);
         return new ResponseEntity<>(savedSession, HttpStatus.CREATED);
     }
 
-    @GetMapping("/userSession")
+    @GetMapping("/v1/userSession")
     public ResponseEntity<List<UserSession>> getAllUserSession() {
         List<UserSession> userSessions = userSessionService.getAllUserSessions();
         return new ResponseEntity<>(userSessions, HttpStatus.OK);
     }
 
     //
-    @GetMapping("/userSession/{userId}")
+    @GetMapping("/v1/userSession/{userId}")
     public ResponseEntity<List<UserSession>> getUserSessionByUserId(@PathVariable Long userId) {
         List<UserSession> userSessions = userSessionService.getUserSessionByUserId(userId);
         return new ResponseEntity<>(userSessions, HttpStatus.OK);
 
     }
 
-    @DeleteMapping("/deleteUserSession/{appId}/{sessionId}")
+    @DeleteMapping("/v3/deleteUserSession/{appId}/{sessionId}")
     public ResponseEntity<Void> deleteUserSessionRow(@PathVariable Long appId, @PathVariable String sessionId) {
         userSessionService.deleteUserSessionRow(appId, sessionId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-//    @DeleteMapping("/userSession/{sessionId}")
+//    @DeleteMapping("/v1/userSession/{sessionId}")
 //    public ResponseEntity<Void> deleteUserSession(@PathVariable String sessionId) {
 //        userSessionService.deleteUserSession(sessionId);
 //        return new ResponseEntity<>(HttpStatus.OK);
