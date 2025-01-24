642649a9083682f62e866d975eb7cee2a3f1f9d7
Rizwan <Rizwan@avanzasolutions.com>
2025-01-24 15:27:32 +0500

    create Api for get license key by app Id

--- src/main/java/com/avanza/license/controller/LicenseKeyController.java
+++ src/main/java/com/avanza/license/controller/LicenseKeyController.java
@@ -1,11 +1,13 @@
 package com.avanza.license.controller;
 
 import com.avanza.license.entity.LicenseKey;
+import com.avanza.license.entity.Module;
 import com.avanza.license.service.LicenseKeyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class LicenseKeyController {
     public ResponseEntity<List<LicenseKey>> getAllLicenseKey() {
         List<LicenseKey> licenseKeys = licenseKeyService.getAllLicenseKeys();
         return new ResponseEntity<>(licenseKeys, HttpStatus.OK);
+    }
+    @GetMapping("/oneLicenseKey/{appId}")
+    public ResponseEntity<LicenseKey> getLicenseKeyByAppId(@PathVariable Long appId) {
+        LicenseKey licenseKey = licenseKeyService.getLicenseKeyByAppId(appId);
+        return new ResponseEntity<>(licenseKey, HttpStatus.OK);
     }
 }
--- src/main/java/com/avanza/license/service/impl/LicenseKeyServiceImpl.java
+++ src/main/java/com/avanza/license/service/impl/LicenseKeyServiceImpl.java
@@ -1,12 +1,16 @@
 package com.avanza.license.service.impl;
 
+import com.avanza.license.Enum.ErrorCode;
 import com.avanza.license.entity.LicenseKey;
+import com.avanza.license.entity.UserTable;
 import com.avanza.license.repositories.LicenseKeyRepository;
 import com.avanza.license.service.LicenseKeyService;
+import com.avanza.license.util.ErrorHandlerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -18,5 +22,13 @@ public class LicenseKeyServiceImpl implements LicenseKeyService {
     @Override
     public List<LicenseKey> getAllLicenseKeys() {
         return licenseKeyRepository.findAll();
+    }
+    @Override
+    public LicenseKey getLicenseKeyByAppId(Long appId) {
+        Optional<LicenseKey> licenseKeyOptional = licenseKeyRepository.findByApplicationAppId(appId);
+        if (!licenseKeyOptional.isPresent()) {
+            ErrorHandlerUtil.handleError(ErrorCode.INVALID_APP_ID);
+        }
+        return licenseKeyOptional.get();
     }
 }
